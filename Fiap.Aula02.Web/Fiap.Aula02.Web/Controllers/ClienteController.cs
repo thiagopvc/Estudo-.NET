22e30762016b3ea1f31aac5aa739a4d7475d7b21
thiagopvc <50061404+thiagopvc@users.noreply.github.com>
2020-08-13 11:57:21 -0300

    Aula 02 .NET

---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web/Controllers/ClienteController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiap.Aula02.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula02.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] //Abre a pagina com o formulario
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost] //Adicionado apos o submit do formulario
+        public IActionResult Cadastrar(Cliente cliente)
+        {
+            //return Content(cliente.Nome + " " + cliente.Sobrenome + " " + cliente.Idade); //retorna um texto
+            //Envia uma informacao para a view
+            ViewData["nome"] = cliente.Nome + " " + cliente.Sobrenome;
+            ViewBag.idade = cliente.Idade;
+            ViewBag.customer = cliente;
+
+            //Mantem a informacao apos um redirect
+            TempData["msg"] = "Cliente cadastrado";
+            
+            //Redirect
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula02.Web.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
